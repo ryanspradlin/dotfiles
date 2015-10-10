28f0899013f0fb365cd44d84fa5eaeed6d48b36c
Ryan Spradlin <ryan@cotangent.org>
2015-10-10 13:56:41 -0500

    Add new path and environment variable setup in config.fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,18 @@
 # Set $PATH
 set --export PATH "/Applications/Sublime Text.app/Contents/SharedSupport/bin" $PATH
+set --export PATH ~/.npm-packages/bin $PATH
 
 # Set $HOSTNAME
 set --export HOSTNAME (hostname)
 
 # Set $EDITOR
 set --export EDITOR "subl --new-window --wait"
+
+# Set JAVA_HOME
+set --export JAVA_HOME (/usr/libexec/java_home)
+
+# Set $PGDATA
+set --export PGDATA "/usr/local/var/postgres"
 
 # Alias to make subl always use new window
 alias subl "subl --new-window"
